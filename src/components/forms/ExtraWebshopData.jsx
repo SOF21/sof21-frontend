b214042c84195be768b78fe0b516911369b949ba
Michelle Krejci <michkrej@gmail.com>
2021-05-02 20:16:39 +0200

    Updated webshop with pickup times

--- src/components/forms/ExtraWebshopData.jsx
+++ src/components/forms/ExtraWebshopData.jsx
@@ -1,29 +1,28 @@
 /* eslint-disable react-hooks/rules-of-hooks */
-import React, { useState } from 'react';
+import React, { useState } from "react"
 
-import { FormattedMessage, injectIntl } from 'react-intl'
+import { FormattedMessage, injectIntl } from "react-intl"
 
-import { GridCell, GridInner } from '@rmwc/grid';
-import { Button } from '@rmwc/button';
+import { GridCell, GridInner } from "@rmwc/grid"
+import { Button } from "@rmwc/button"
 
 import {
   Dialog,
   DialogTitle,
   DialogContent,
   DialogActions,
-  DialogButton
-} from '@rmwc/dialog';
+  DialogButton,
+} from "@rmwc/dialog"
 
-import FormTextInput from '../../components/forms/components/FormTextInput';
-import { Select } from '@rmwc/select';
-import { Formik, Form } from 'formik/dist/index';
-import * as Yup from 'yup';
+import FormTextInput from "../../components/forms/components/FormTextInput"
+import { Select } from "@rmwc/select"
+import { Formik, Form } from "formik/dist/index"
+import * as Yup from "yup"
 
-import { getCurrentUser } from '../../api/userCalls'
-import { addExtraWebshopData } from '../../api/ticketPickupCalls'
+import { getCurrentUser } from "../../api/userCalls"
+import { addExtraWebshopData } from "../../api/ticketPickupCalls"
 
 const extraWebshopDataComponent = (props) => {
-
   const phoneRegExp = /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/
   const [dialogOpen, setOpen] = useState(false)
 
@@ -31,7 +30,7 @@ const extraWebshopDataComponent = (props) => {
     getCurrentUser().then((response) => {
       addExtraWebshopData({
         id: response.data.id,
-        ...values
+        ...values,
       })
     })
   }
@@ -45,14 +44,24 @@ const extraWebshopDataComponent = (props) => {
       </GridCell>
       <GridCell desktop='12' tablet='8' phone='4'>
         <Formik
-          initialValues={{ adress: '', phone: '' }}
+          initialValues={{ adress: "", phone: "" }}
           validationSchema={Yup.object().shape({
-            adress: Yup.string().required('Du måste ange en address'),
-            phone: Yup.string().matches(phoneRegExp, 'Mobilnummret är inte korrekt').required('Du måste ange ett mobilnummer'),
+            adress: Yup.string().required("Du måste ange en address"),
+            phone: Yup.string()
+              .matches(phoneRegExp, "Mobilnummret är inte korrekt")
+              .required("Du måste ange ett mobilnummer"),
           })}
           onSubmit={handleSubmit}
-          render={({ values, handleChange, handleBlur, errors, touched, isValid, isSubmitting }) => (
-            <Form style={{ width: '100%' }} >
+          render={({
+            values,
+            handleChange,
+            handleBlur,
+            errors,
+            touched,
+            isValid,
+            isSubmitting,
+          }) => (
+            <Form style={{ width: "100%" }}>
               <Dialog
                 open={dialogOpen}
                 onClose={() => setOpen(false)}
@@ -64,15 +73,31 @@ const extraWebshopDataComponent = (props) => {
                 <DialogContent>
                   <GridInner>
                     <GridCell desktop='12' tablet='8' phone='4'>
-                      Nedan kan du fylla i information som krävs för att kunna köpa saker i webshopen.
+                      <p><b>Tider för upphämtning:</b></p>
+                      <ul style={{ textAlign: "left", fontSize: "14px" }}>
+                        <li>Ryd [HG] (10:00 - 13:00)</li>
+                        <li>Colonia (10:00 - 10:30)</li>
+                        <li>Vallastaden AutoMat (10:35 - 11:05),</li>
+                        <li>VilleValla (11:15 - 11:45)</li>
+                        <li>Flamman (11:50 - 12.30)</li>
+                        <li>
+                          Katedralskolan, norra parkeringen (12.25 - 12:55)
+                        </li>
+                        <li>Trappan (13:35 - 14:05)</li>
+                      </ul>
+                      <p>Om ingen av dessa tider fungerar går det att hämta paketet i Ryd vid 15:00, välj 'Ryd [hg] (15:00)' i listan</p>
                     </GridCell>
-                    {errors.global && <GridCell desktop='12' tablet='8' phone='4'> {errors.global}</GridCell>}
+                    {errors.global && (
+                      <GridCell desktop='12' tablet='8' phone='4'>
+                        {" "}
+                        {errors.global}
+                      </GridCell>
+                    )}
                     <GridCell desktop='12' tablet='8' phone='4'>
                       <FormTextInput
-                        style={{ width: '100%' }}
+                        style={{ width: "100%" }}
                         name='phone'
                         label={"Mobilnummer"}
-
                         value={values.phone}
                         error={errors.phone}
                         touched={touched.phone}
@@ -85,34 +110,37 @@ const extraWebshopDataComponent = (props) => {
                         id='adress'
                         options={[
                           {
-                            label: 'Linköping',
+                            label: "Linköping",
                             options: [
-                              'Ryd [HG]',
-                              'Colonia',
-                              'Vallastaden',
-                              'Irrblosset',
-                              'Flamman',
-                              'Katedralskolans norra parkering',
-                            ]
+                              "Ryd [hg] (10:00 - 13:00)",
+                              "Colonia (10:00 - 10:30)",
+                              "Vallastaden AutoMat (10:35 - 11:05)",
+                              "VilleValla (11:15 - 11:45)",
+                              "Flamman (11:50 - 12.30)",
+                              "Katedralskolan, norra parkeringen (12.25 - 12:55)",
+                              "Ryd [hg] (15:00)",
+                            ],
                           },
                           {
-                            label: 'Norrköping',
-                            options: [
-                              'Trappan'
-                            ]
-                          }
+                            label: "Norrköping",
+                            options: ["Trappan (13:35 - 14:05)"],
+                          },
                         ]}
-                        value={values.adress || ''}
+                        value={values.adress || ""}
                         onChange={handleChange}
-                        />
+                      />
                     </GridCell>
                   </GridInner>
                 </DialogContent>
                 <DialogActions>
-                  <DialogButton action="close" type='button' isDefaultAction >
+                  <DialogButton action='close' type='button' isDefaultAction>
                     Avbryt
                   </DialogButton>
-                  <DialogButton action='close' type='submit' disabled={!isValid} >
+                  <DialogButton
+                    action='close'
+                    type='submit'
+                    disabled={!isValid}
+                  >
                     Skicka
                   </DialogButton>
                 </DialogActions>
--- src/pages/base_pages/Shop.jsx
+++ src/pages/base_pages/Shop.jsx
@@ -1,37 +1,37 @@
-import React, { Component, setState } from 'react';
+import React, { Component, setState } from "react"
 
-import ArticleCard from '../../components/page_components/ArticleCard';
+import ArticleCard from "../../components/page_components/ArticleCard"
 
-import { FormattedMessage, injectIntl } from 'react-intl'
-import { Grid, GridCell, GridInner } from '@rmwc/grid';
+import { FormattedMessage, injectIntl } from "react-intl"
+import { Grid, GridCell, GridInner } from "@rmwc/grid"
 
-import { withRouter } from 'react-router-dom';
+import { withRouter } from "react-router-dom"
 
-import { fetchProducts } from '../../actions/shop';
-import { addProductToCart } from '../../actions/cart';
+import { fetchProducts } from "../../actions/shop"
+import { addProductToCart } from "../../actions/cart"
 
-import { connect } from 'react-redux'
+import { connect } from "react-redux"
 
-import Header from '../../components/page_components/NiceHeader';
+import Header from "../../components/page_components/NiceHeader"
 
-import { getCurrentUser } from '../../api/userCalls'
+import { getCurrentUser } from "../../api/userCalls"
 
 class Shop extends Component {
   constructor(props) {
-    super(props);
-    this.intl = this.props.intl;
+    super(props)
+    this.intl = this.props.intl
 
     this.state = {
       hasShippingInfo: false,
     }
-  };
+  }
 
   componentDidMount() {
-    getCurrentUser().then(response => {
+    getCurrentUser().then((response) => {
       if (response.data.phone !== null && response.data.pick_up_point !== null)
         this.setState({ hasShippingInfo: true })
     })
-  };
+  }
 
   static pageTitle() {
     return <FormattedMessage id='Shop.title' />
@@ -42,57 +42,76 @@ class Shop extends Component {
   }
 
   render() {
-    var articles = null;
-    if (!this.props.isLoading && this.props.products && this.state.hasShippingInfo) {
-      articles = this.props.products.map(article => (
+    var articles = null
+    if (
+      !this.props.isLoading &&
+      this.props.products &&
+      this.state.hasShippingInfo
+    ) {
+      articles = this.props.products.map((article) => (
         <GridCell phone='4' tablet='4' desktop='6'>
           <ArticleCard
             article={article}
             addCallback={(id) => this.props.addProductToCart(id)}
           />
         </GridCell>
-      ));
+      ))
     }
 
     console.log(this.props.user)
     return (
       <React.Fragment>
-        <Grid className="base-outer-grid base-outer-grid--first">
+        <Grid className='base-outer-grid base-outer-grid--first'>
           <GridInner>
-            <GridCell phone='4' tablet='8' desktop='12' style={{ textAlign: 'center' }}>
-              Dina produkter kommer att levereras till den plats du valde i din profil.
-              {!this.state.hasShippingInfo &&
-                <p style={{ color: 'red' }}>För att handla i webshopen behöver du lägga till adress, mobilnummer på ditt konto.
-                Det gör du genom att gå in på 'Mitt konto' och trycka på knappen den röda knappen längst ner på sidan.
+            <GridCell
+              phone='4'
+              tablet='8'
+              desktop='12'
+              style={{ textAlign: "center" }}
+            >
+              Dina produkter kommer att levereras till den plats du valde i din
+              profil.
+              {!this.state.hasShippingInfo && (
+                <p style={{ color: "red" }}>
+                  För att handla i webshopen behöver du lägga till adress,
+                  mobilnummer på ditt konto. Det gör du genom att gå in på 'Mitt
+                  konto' och trycka på knappen den röda knappen längst ner på
+                  sidan.
                 </p>
-              }
+              )}
             </GridCell>
             <GridCell phone='4' tablet='4' desktop='6'>
-              <p style={{ textAlign: 'center' }}><b>Linköping</b></p>
+              <p style={{ textAlign: "center" }}>
+                <b>Linköping</b>
+              </p>
               <ul>
-                <li>Ryd [HG]</li>
-                <li>Colonia</li>
-                <li>Irrblosset</li>
-                <li>Flamman</li>
-                <li>Katedralskolan, norra parkeringen</li>
+                <li>Ryd [HG] (10:00 - 13:00)</li>
+                <li>Colonia (10:00 - 10:30)</li>
+                <li>Vallastaden AutoMat (10:35 - 11:05),</li>
+                <li>VilleValla (11:15 - 11:45)</li>
+                <li>Flamman (11:50 - 12.30)</li>
+                <li>Katedralskolan, norra parkeringen (12.25 - 12:55)</li>
               </ul>
             </GridCell>
             <GridCell phone='4' tablet='4' desktop='6'>
-              <p style={{ textAlign: 'center' }}><b>Norrköping</b></p>
+              <p style={{ textAlign: "center" }}>
+                <b>Norrköping</b>
+              </p>
               <ul>
-                <li>Trappan</li>
+                <li>Trappan (13:35 - 14:05)</li>
               </ul>
+            </GridCell>
+            <GridCell phone='4' tablet='8' desktop='12' style={{ textAlign: "center" }}>
+              <p>Möjlighet för sen upphämtning finns i Ryd vid 15:00</p>
             </GridCell>
             <GridCell phone='4' tablet='8' desktop='12'>
-              <Header>
-                Produkter
-              </Header>
+              <Header>Produkter</Header>
             </GridCell>
-            {(!this.props.isLoading && this.props.products) ? articles : null}
+            {!this.props.isLoading && this.props.products ? articles : null}
           </GridInner>
         </Grid>
       </React.Fragment>
-    );
+    )
   }
 }
 
@@ -100,9 +119,10 @@ const mapStateToProps = (state) => {
   return {
     products: state.shop.products,
     isLoading: state.shop.loading,
-    user: state
-
-  };
+    user: state,
+  }
 }
 
-export default connect(mapStateToProps, { fetchProducts, addProductToCart })(withRouter(injectIntl(Shop, { forwardRef: true })));
+export default connect(mapStateToProps, { fetchProducts, addProductToCart })(
+  withRouter(injectIntl(Shop, { forwardRef: true }))
+)
