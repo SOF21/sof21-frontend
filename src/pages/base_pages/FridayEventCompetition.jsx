a3330a813be59e4753a26c9ded5d4f8ab499d586
Michelle Krejci <michkrej@gmail.com>
2021-05-10 21:14:52 +0200

    Hid page content if password not entered

--- src/pages/base_pages/FridayEventCompetition.jsx
+++ src/pages/base_pages/FridayEventCompetition.jsx
@@ -1,9 +1,11 @@
 import React, { Component } from "react"
-import { FormattedMessage, injectIntl } from "react-intl"
-import Header from "../../components/page_components/NiceHeader"
 
 import { Grid, GridCell, GridInner } from "@rmwc/grid"
 import Button from "@rmwc/button"
+import FormTextInput from "../../components/forms/components/FormTextInput"
+import Header from "../../components/page_components/NiceHeader"
+
+import { info } from "../../constants"
 
 const content = [
   {
@@ -84,154 +86,190 @@ const content = [
 ]
 
 class FridayEventCompInfo extends Component {
+  constructor(props) {
+    super()
+    this.state = {
+      code: "",
+      open: false,
+    }
+  }
+
   static pageTitle() {
-    return <FormattedMessage id='Uppdrag' />
+    return "Uppdrag"
   }
 
   static pageNavTitle() {
-    return <FormattedMessage id='Uppdrag' />
+    return "Uppdrag"
+  }
+
+  handlePasscode = (e) => {
+    e.preventDefault()
+    if (this.state.code === info.SOFFriday) {
+      this.setState({ open: true })
+    }
   }
 
   render() {
     return (
       <React.Fragment>
-        <Grid className='base-outer-grid '>
-          <GridInner>
-            <GridCell phone='4' tablet='8' desktop='12'>
-              <Header>Frågor</Header>
+        {!this.state.open && (
+          <Grid className='base-outer-grid '>
+            <GridCell desktop='12' tablet='8' phone='4'>
+              <form onSubmit={this.handlePasscode}>
+                <FormTextInput
+                  label='Adminkod'
+                  type='text'
+                  onChange={(e) => this.setState({ code: e.target.value })}
+                  onSubmit={(e) => this.handlePasscode(e)}
+                  value={this.state.code}
+                  style={{ width: "100%", marginBottom: "1em" }}
+                />
+                <Button type='submit' raised style={{ width: "100%" }}>
+                  Skicka
+                </Button>
+              </form>
             </GridCell>
-            <GridCell
-              phone='4'
-              tablet='8'
-              desktop='12'
-              style={{ padding: "1.2em" }}
-            >
-              <Grid>
-                <GridCell phone='4' tablet='4' desktop='4'>
-                  <Button
-                    raised
-                    tag='a'
-                    href='https://docs.google.com/forms/d/1FLXPbFPYUlhHHGqhtRcegH0Fzak5HTORBbJD0usxIHM/edit?usp=sharing '
-                    target='_blank'
-                    rel='noopener noreferrer'
-                    style={{ width: "100%" }}
-                  >
-                    Geografi
-                  </Button>
-                </GridCell>
-                <GridCell phone='4' tablet='4' desktop='4'>
-                  <Button
-                    raised
-                    tag='a'
-                    href='https://docs.google.com/forms/d/1zyU4zJtC73rk0AjrPdXUrgYecMwYedcR7agScIGwQLo/edit?usp=sharing'
-                    target='_blank'
-                    rel='noopener noreferrer'
-                    style={{ width: "100%" }}
-                  >
-                    Underhållning
-                  </Button>
-                </GridCell>
-
-                <GridCell phone='4' tablet='4' desktop='4'>
-                  <Button
-                    raised
-                    tag='a'
-                    href='https://docs.google.com/forms/d/1W6UmtBiSy1gSk0icVFGieOLLX17Geo0M-D9GbI3DNlA/edit?usp=sharing'
-                    target='_blank'
-                    rel='noopener noreferrer'
-                    style={{ width: "100%" }}
-                  >
-                    Historia
-                  </Button>
-                </GridCell>
-                <GridCell phone='4' tablet='4' desktop='4'>
-                  <Button
-                    raised
-                    tag='a'
-                    href='https://docs.google.com/forms/d/1oJ51cf9PcB8M08ZILEIXBjOVFW65gSwboOvIE-IKZdY/edit?usp=sharing '
-                    target='_blank'
-                    rel='noopener noreferrer'
-                    style={{ width: "100%" }}
-                  >
-                    Naturvetenskap
-                  </Button>
-                </GridCell>
-
-                <GridCell phone='4' tablet='4' desktop='4'>
-                  <Button
-                    raised
-                    tag='a'
-                    href='https://docs.google.com/forms/d/1hnofrBu5_w7BLYT9avYfRGDA0OCSKxOjSZlj-ms38qk/edit?usp=sharing '
-                    target='_blank'
-                    rel='noopener noreferrer'
-                    style={{ width: "100%" }}
-                  >
-                    Sport & Fritid
-                  </Button>
-                </GridCell>
-
-                <GridCell phone='4' tablet='4' desktop='4'>
-                  <Button
-                    raised
-                    tag='a'
-                    href='https://docs.google.com/forms/d/1reGUg4iXSvyjeRM-jNCdyjj9WDA-JDeZmlnt0DRLGsw/edit?usp=sharing '
-                    target='_blank'
-                    rel='noopener noreferrer'
-                    style={{ width: "100%", textAlign: "center" }}
-                  >
-                    Kultur & litteratur
-                  </Button>
-                </GridCell>
-              </Grid>
-            </GridCell>
-            <GridCell phone='4' tablet='8' desktop='12'>
-              <Header>Uppdrag</Header>
-            </GridCell>
-            <GridCell phone='4' tablet='8' desktop='12'>
-              <div
-                style={{
-                  display: "grid",
-                  gridTemplateColumns: "1fr",
-                  gridGap: "1em",
-                }}
+          </Grid>
+        )}
+        {this.state.open && (
+          <Grid className='base-outer-grid '>
+            <GridInner>
+              <GridCell phone='4' tablet='8' desktop='12'>
+                <Header>Frågor</Header>
+              </GridCell>
+              <GridCell
+                phone='4'
+                tablet='8'
+                desktop='12'
+                style={{ padding: "1.2em" }}
               >
-                {content.map((category) => {
-                  const events = category.events.map((event) => {
+                <Grid>
+                  <GridCell phone='4' tablet='4' desktop='4'>
+                    <Button
+                      raised
+                      tag='a'
+                      href='https://docs.google.com/forms/d/1FLXPbFPYUlhHHGqhtRcegH0Fzak5HTORBbJD0usxIHM/edit?usp=sharing '
+                      target='_blank'
+                      rel='noopener noreferrer'
+                      style={{ width: "100%" }}
+                    >
+                      Geografi
+                    </Button>
+                  </GridCell>
+                  <GridCell phone='4' tablet='4' desktop='4'>
+                    <Button
+                      raised
+                      tag='a'
+                      href='https://docs.google.com/forms/d/1zyU4zJtC73rk0AjrPdXUrgYecMwYedcR7agScIGwQLo/edit?usp=sharing'
+                      target='_blank'
+                      rel='noopener noreferrer'
+                      style={{ width: "100%" }}
+                    >
+                      Underhållning
+                    </Button>
+                  </GridCell>
+
+                  <GridCell phone='4' tablet='4' desktop='4'>
+                    <Button
+                      raised
+                      tag='a'
+                      href='https://docs.google.com/forms/d/1W6UmtBiSy1gSk0icVFGieOLLX17Geo0M-D9GbI3DNlA/edit?usp=sharing'
+                      target='_blank'
+                      rel='noopener noreferrer'
+                      style={{ width: "100%" }}
+                    >
+                      Historia
+                    </Button>
+                  </GridCell>
+                  <GridCell phone='4' tablet='4' desktop='4'>
+                    <Button
+                      raised
+                      tag='a'
+                      href='https://docs.google.com/forms/d/1oJ51cf9PcB8M08ZILEIXBjOVFW65gSwboOvIE-IKZdY/edit?usp=sharing '
+                      target='_blank'
+                      rel='noopener noreferrer'
+                      style={{ width: "100%" }}
+                    >
+                      Naturvetenskap
+                    </Button>
+                  </GridCell>
+
+                  <GridCell phone='4' tablet='4' desktop='4'>
+                    <Button
+                      raised
+                      tag='a'
+                      href='https://docs.google.com/forms/d/1hnofrBu5_w7BLYT9avYfRGDA0OCSKxOjSZlj-ms38qk/edit?usp=sharing '
+                      target='_blank'
+                      rel='noopener noreferrer'
+                      style={{ width: "100%" }}
+                    >
+                      Sport & Fritid
+                    </Button>
+                  </GridCell>
+
+                  <GridCell phone='4' tablet='4' desktop='4'>
+                    <Button
+                      raised
+                      tag='a'
+                      href='https://docs.google.com/forms/d/1reGUg4iXSvyjeRM-jNCdyjj9WDA-JDeZmlnt0DRLGsw/edit?usp=sharing '
+                      target='_blank'
+                      rel='noopener noreferrer'
+                      style={{ width: "100%", textAlign: "center" }}
+                    >
+                      Kultur & litteratur
+                    </Button>
+                  </GridCell>
+                </Grid>
+              </GridCell>
+              <GridCell phone='4' tablet='8' desktop='12'>
+                <Header>Uppdrag</Header>
+              </GridCell>
+              <GridCell phone='4' tablet='8' desktop='12'>
+                <div
+                  style={{
+                    display: "grid",
+                    gridTemplateColumns: "1fr",
+                    gridGap: "1em",
+                  }}
+                >
+                  {content.map((category) => {
+                    const events = category.events.map((event) => {
+                      return (
+                        <li
+                          style={{ marginBottom: "0.5em" }}
+                          dangerouslySetInnerHTML={{ __html: event }}
+                        />
+                      )
+                    })
                     return (
-                      <li
-                        style={{ marginBottom: "0.5em" }}
-                        dangerouslySetInnerHTML={{ __html: event }}
-                      />
+                      <Grid>
+                        <GridCell
+                          phone='4'
+                          tablet='3'
+                          desktop='3'
+                          style={{
+                            display: "flex",
+                            alignItems: "center",
+                            justifyContent: "center",
+                            textAlign: "center",
+                          }}
+                        >
+                          <h5>{category.title}</h5>
+                        </GridCell>
+                        <GridCell phone='4' tablet='5' desktop='9'>
+                          <ul>{events}</ul>
+                        </GridCell>
+                      </Grid>
                     )
-                  })
-                  return (
-                    <Grid>
-                      <GridCell
-                        phone='4'
-                        tablet='3'
-                        desktop='3'
-                        style={{
-                          display: "flex",
-                          alignItems: "center",
-                          justifyContent: "center",
-                          textAlign: "center",
-                        }}
-                      >
-                        <h5>{category.title}</h5>
-                      </GridCell>
-                      <GridCell phone='4' tablet='5' desktop='9'>
-                        <ul>{events}</ul>
-                      </GridCell>
-                    </Grid>
-                  )
-                })}
-              </div>
-            </GridCell>
-          </GridInner>
-        </Grid>
+                  })}
+                </div>
+              </GridCell>
+            </GridInner>
+          </Grid>
+        )}
       </React.Fragment>
     )
   }
 }
 
-export default injectIntl(FridayEventCompInfo)
+export default FridayEventCompInfo
--- src/pages/base_pages/FridayEventInfo.jsx
+++ src/pages/base_pages/FridayEventInfo.jsx
@@ -1,111 +1,150 @@
 import React, { Component } from "react"
-import { FormattedMessage, injectIntl } from "react-intl"
-import Header from "../../components/page_components/NiceHeader"
-
-import { connect, connectAdvanced } from "react-redux"
 
 import { Grid, GridCell, GridInner } from "@rmwc/grid"
+import Button from "@rmwc/button"
+import FormTextInput from "../../components/forms/components/FormTextInput"
+import Header from "../../components/page_components/NiceHeader"
+
+import { info } from "../../constants"
 
 class FridayEventInfo extends Component {
+  constructor(props) {
+    super()
+    this.state = {
+      code: "",
+      open: false,
+    }
+  }
+
   static pageTitle() {
-    return <FormattedMessage id='Information' />
+    return "Information"
   }
 
   static pageNavTitle() {
-    return <FormattedMessage id='Information' />
+    return "Information"
+  }
+
+  handlePasscode = (e) => {
+    e.preventDefault()
+    if (this.state.code === info.SOFFriday) {
+      this.setState({ open: true })
+    }
   }
 
   render() {
     return (
       <React.Fragment>
-        <Grid className='base-outer-grid '>
-          <GridInner>
-            <GridCell
-              phone='4'
-              tablet='8'
-              desktop='12'
-              style={{ padding: "1.2em" }}
-            >
-              <p>
-                Mångkampen är en tävling som hålls under SOF-Fredagen mellan kl
-                10:00-16:00. Vinnaren kommer sedan att presenteras under
-                sittningen. För att delta skapar du ett lag med en eller flera
-                personer genom att skapa ett öppet instagramkonto eller använda
-                ett redan existerande. Laget ska sedan anmäla sig i
-                <a href='https://forms.gle/9QewKDZJAHV7FgXy8'>
-                  {" "}
-                  detta formulär{" "}
-                </a>
-                och utföra uppdragen som står på under SOF-Fredag -> Uppdrag.
-                När laget är anmält kommer lagnamn och länk till instagramkontot
-                läggas upp på hemsidan under SOF-Fredag -> Laginfo. Glöm inte
-                att följa
-                <a href='https://www.instagram.com/studentorkesterfestivalen/'>
-                  {" "}
-                  @studentorkesterfestivalen{" "}
-                </a>
-                på Instagram!
-              </p>
-
-              <p>
-                Uppdragen är uppdelade i frågor och utmaningar. Under varje
-                kategori finns 6 teman: geografi, underhållning, historia,
-                naturvetenskap, kultur & litteratur och sport & fritid. Klara så
-                många uppdrag som du kan innan tiden är slut. Frågorna får bara
-                skickas in en gång per lag.{" "}
-              </p>
-
-              <p>
-                Försök att inte träffa folk och tänk på att följa
-                Folkhälsomyndighetens restriktioner och de rekommendationer som
-                finns i din region. Ingen av uppgifterna ska utföras om det
-                finns risk för att skada sig. Deltagande sker på egen risk. Ha
-                kul!
-              </p>
-
-              <p>Att göra:</p>
-              <ol>
-                <li>
-                  Hitta lag, Behöver du hjälp att hitta lagkamrater gå in här:
-                  ???
-                </li>
-                <li>
-                  Skapa ett publikt instagramkonto eller använd ett befintligt
-                </li>
-                <li>
-                  Följ{" "}
+        {!this.state.open && (
+          <Grid className='base-outer-grid '>
+            <GridCell desktop='12' tablet='8' phone='4'>
+              <form onSubmit={this.handlePasscode}>
+                <FormTextInput
+                  label='Adminkod'
+                  type='text'
+                  onChange={(e) => this.setState({ code: e.target.value })}
+                  onSubmit={(e) => this.handlePasscode(e)}
+                  value={this.state.code}
+                  style={{ width: "100%", marginBottom: "1em" }}
+                />
+                <Button type='submit' raised style={{ width: "100%" }}>
+                  Skicka
+                </Button>
+              </form>
+            </GridCell>
+          </Grid>
+        )}
+        {this.state.open && (
+          <Grid className='base-outer-grid '>
+            <GridInner>
+              <GridCell
+                phone='4'
+                tablet='8'
+                desktop='12'
+                style={{ padding: "1.2em" }}
+              >
+                <p>
+                  Mångkampen är en tävling som hålls under SOF-Fredagen mellan
+                  kl 10:00-16:00. Vinnaren kommer sedan att presenteras under
+                  sittningen. För att delta skapar du ett lag med en eller flera
+                  personer genom att skapa ett öppet instagramkonto eller
+                  använda ett redan existerande. Laget ska sedan anmäla sig i
+                  <a href='https://forms.gle/9QewKDZJAHV7FgXy8'>
+                    {" "}
+                    detta formulär{" "}
+                  </a>
+                  och utföra uppdragen som står på under SOF-Fredag -> Uppdrag.
+                  När laget är anmält kommer lagnamn och länk till
+                  instagramkontot läggas upp på hemsidan under SOF-Fredag ->
+                  Laginfo. Glöm inte att följa
                   <a href='https://www.instagram.com/studentorkesterfestivalen/'>
                     {" "}
                     @studentorkesterfestivalen{" "}
-                  </a>{" "}
-                  på instagram
-                </li>
-                <li>
-                  <a href='https://forms.gle/9QewKDZJAHV7FgXy8'>Anmäl laget</a>
-                </li>
-                <li>Börja med uppdragen!</li>
-              </ol>
+                  </a>
+                  på Instagram!
+                </p>
 
-              <p>Är det några frågor skriv till SOF på instagram. </p>
+                <p>
+                  Uppdragen är uppdelade i frågor och utmaningar. Under varje
+                  kategori finns 6 teman: geografi, underhållning, historia,
+                  naturvetenskap, kultur & litteratur och sport & fritid. Klara
+                  så många uppdrag som du kan innan tiden är slut. Frågorna får
+                  bara skickas in en gång per lag.{" "}
+                </p>
 
-              <p style={{ marginBottom: 0 }}>
-                <b>Snabb info</b>
-              </p>
-              <p style={{ marginTop: 0 }}>
-                Dag: 14 maj <br />
-                Tid: 10:00 - 16:00 <br />
-                Uppdragen: ??? <br />
-                Hitta lag: ??? <br />
-                Vinnaren prsenteras på sittningarna. <br />
-                Pris: En högtalare och lite annat smått och gott.
-              </p>
-              <p>Lycka till!</p>
-            </GridCell>
-          </GridInner>
-        </Grid>
+                <p>
+                  Försök att inte träffa folk och tänk på att följa
+                  Folkhälsomyndighetens restriktioner och de rekommendationer
+                  som finns i din region. Ingen av uppgifterna ska utföras om
+                  det finns risk för att skada sig. Deltagande sker på egen
+                  risk. Ha kul!
+                </p>
+
+                <p>Att göra:</p>
+                <ol>
+                  <li>
+                    Hitta lag, Behöver du hjälp att hitta lagkamrater gå in här:
+                    ???
+                  </li>
+                  <li>
+                    Skapa ett publikt instagramkonto eller använd ett befintligt
+                  </li>
+                  <li>
+                    Följ{" "}
+                    <a href='https://www.instagram.com/studentorkesterfestivalen/'>
+                      {" "}
+                      @studentorkesterfestivalen{" "}
+                    </a>{" "}
+                    på instagram
+                  </li>
+                  <li>
+                    <a href='https://forms.gle/9QewKDZJAHV7FgXy8'>
+                      Anmäl laget
+                    </a>
+                  </li>
+                  <li>Börja med uppdragen!</li>
+                </ol>
+
+                <p>Är det några frågor skriv till SOF på instagram. </p>
+
+                <p style={{ marginBottom: 0 }}>
+                  <b>Snabb info</b>
+                </p>
+                <p style={{ marginTop: 0 }}>
+                  Dag: 14 maj <br />
+                  Tid: 10:00 - 16:00 <br />
+                  Uppdragen: ??? <br />
+                  Hitta lag: ??? <br />
+                  Vinnaren prsenteras på sittningarna. <br />
+                  Pris: En högtalare och lite annat smått och gott.
+                </p>
+                <p>Lycka till!</p>
+              </GridCell>
+            </GridInner>
+          </Grid>
+        )}
       </React.Fragment>
     )
   }
 }
 
-export default injectIntl(FridayEventInfo)
+export default FridayEventInfo
