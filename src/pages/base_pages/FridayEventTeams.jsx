780bedd72896ad0ae84ed2fa53bd92a0c001b6c0
Michelle Krejci <michkrej@gmail.com>
2021-05-13 12:24:56 +0200

    Small changes to sof-friday

--- src/pages/base_pages/FridayEventTeams.jsx
+++ src/pages/base_pages/FridayEventTeams.jsx
@@ -31,53 +31,25 @@ class FridayEventTeams extends Component {
     return "Lag"
   }
 
-  handlePasscode = (e) => {
-    e.preventDefault()
-    if (this.state.code === info.SOFFriday) {
-      this.setState({ open: true })
-    }
-  }
-
   render() {
     return (
       <React.Fragment>
-        {!this.state.open && (
-          <Grid className='base-outer-grid '>
-            <GridCell desktop='12' tablet='8' phone='4'>
-              <form onSubmit={this.handlePasscode}>
-                <FormTextInput
-                  label='Adminkod'
-                  type='text'
-                  onChange={(e) => this.setState({ code: e.target.value })}
-                  onSubmit={(e) => this.handlePasscode(e)}
-                  value={this.state.code}
-                  style={{ width: "100%", marginBottom: "1em" }}
-                />
-                <Button type='submit' raised style={{ width: "100%" }}>
-                  Skicka
-                </Button>
-              </form>
-            </GridCell>
-          </Grid>
-        )}
-        {this.state.open && (
-          <Grid className='base-outer-grid '>
-            <GridInner style={{ padding: "1.2em" }}>
-              {teams.map((team) => (
-                <GridCell
-                  phone='4'
-                  tablet='2'
-                  desktop='3'
-                  style={{ textAlign: "center" }}
-                >
-                  <h5>
-                    <a href={team.link}>{team.name}</a>
-                  </h5>
-                </GridCell>
-              ))}
-            </GridInner>
-          </Grid>
-        )}
+        <Grid className='base-outer-grid '>
+          <GridInner style={{ padding: "1.2em" }}>
+            {teams.map((team) => (
+              <GridCell
+                phone='4'
+                tablet='2'
+                desktop='3'
+                style={{ textAlign: "center" }}
+              >
+                <h5>
+                  <a href={team.link}>{team.name}</a>
+                </h5>
+              </GridCell>
+            ))}
+          </GridInner>
+        </Grid>
       </React.Fragment>
     )
   }
